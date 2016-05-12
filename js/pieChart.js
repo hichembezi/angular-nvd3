46ce152f8a76144c589f45fe2745a09dc3514437
hichembezi <hichembezi@gmail.com>
2016-05-12 16:50:38 +0200

    -
    -

--- js/pieChart.js
+++ js/pieChart.js
@@ -8,8 +8,8 @@ angular.module('mainApp.controllers')
             chart: {
                 type: 'pieChart',
                 height: 500,
-                x: function(d){return d.key;},
-                y: function(d){return d.y;},
+                x: function(d){return d.Nationalite;},
+                y: function(d){return d.NuitsN;},
                 showLabels: true,
                 duration: 500,
                 labelThreshold: 0.01,
@@ -26,33 +26,45 @@ angular.module('mainApp.controllers')
         };
 
         $scope.data = [
-            {
-                key: "One",
-                y: 5
-            },
-            {
-                key: "Two",
-                y: 2
-            },
-            {
-                key: "Three",
-                y: 9
-            },
-            {
-                key: "Four",
-                y: 7
-            },
-            {
-                key: "Five",
-                y: 4
-            },
-            {
-                key: "Six",
-                y: 3
-            },
-            {
-                key: "Seven",
-                y: .5
-            }
-        ];
+  {
+    "Nationalite": "BE",
+    "NuitsN": 1476
+  },
+  {
+    "Nationalite": "CH",
+    "NuitsN": 656
+  },
+  {
+    "Nationalite": "DE",
+    "NuitsN": 6223
+  },
+  {
+    "Nationalite": "DIV",
+    "NuitsN": 2961
+  },
+  {
+    "Nationalite": "ES",
+    "NuitsN": 2000
+  },
+  {
+    "Nationalite": "FR",
+    "NuitsN": 12171
+  },
+  {
+    "Nationalite": "GB",
+    "NuitsN": 16427
+  },
+  {
+    "Nationalite": "IE",
+    "NuitsN": 490
+  },
+  {
+    "Nationalite": "IT",
+    "NuitsN": 89
+  },
+  {
+    "Nationalite": "NL",
+    "NuitsN": 6903
+  }
+];
     })
